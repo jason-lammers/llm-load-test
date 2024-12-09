27ac054e5f570aa6650b12e868b91b823ab6fd2d
jason-lammers <jason@jasonlammers.dev>
2024-12-09 16:27:50 -0500

    Runs llm-load-test against all models running in cluster
    Signed-off-by: Isaiah Stapleton <istaplet@redhat.com>

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # llm-load-test-exporter
 
-The purpose of this program is to run llm-load-test application and then serve the resulting metrics to a /metrics endpoint. 
+The purpose of this program is to run [llm-load-test](https://github.com/openshift-psap/llm-load-test) application and then serve the resulting metrics to a /metrics endpoint. 
--- llm-load-test-exporter.py
+++ llm-load-test-exporter.py
@@ -1,13 +1,14 @@
 from flask import Flask, request, jsonify
-from prometheus_client import start_http_server, Summary
 import subprocess
 from prometheus_client import Gauge, generate_latest, CollectorRegistry
 import os
 import json
+import yaml
+from kubernetes import config, client
+import logging
 
 
 metrics = Flask(__name__)
-
 registry = CollectorRegistry()
 
 itl_metric = Gauge("itl", "Inter-token Latency (ms)", registry=registry)
@@ -15,18 +16,68 @@ ttft_metric = Gauge("ttft", "Time to First Token (ms)", registry=registry)
 response_time_metric = Gauge("response_time", "Response Time (ms)", registry=registry)
 throughput_metric = Gauge("throughput", "Throughput (requests/sec)", registry=registry)
 
+LOG = logging.getLogger(__name__)
+logging.basicConfig(level=logging.INFO)
+
+try:
+    config.load_kube_config()
+except config.ConfigException as e:
+    LOG.error("Could not configure Kubernetes client: %s", str(e))
+    exit(1)
+
+v1 = client.CoreV1Api()
+
+
+# Configure llm-load-test config.yaml
+def set_config(model_name, host_url, namespace):
+    config_path = "./llm-load-test/config.yaml"
+
+    with open(config_path, "r") as file:
+        try:
+            config = yaml.safe_load(file)
+        except Exception as e:
+            print(f"Error loading config.yaml file: {e}")
+            return
+
+    config["plugin_options"]["model_name"] = model_name
+    config["plugin_options"]["host"] = host_url
+    config["output"]["file"] = f"{model_name}-{namespace}"
+
+    with open(config_path, "w") as file:
+        try:
+            config = yaml.dump(config, file)
+        except Exception as e:
+            print(f"Error writing config.yaml file: {e}")
+            return
+
+
+# Gather model information for the config.yaml
+def gather_model_info():
+    model_pods = v1.list_pod_for_all_namespaces(
+        label_selector="serving.kserve.io/inferenceservice"
+    )
+
+    for pod in model_pods.items:
+        model_name = pod.metadata.labels["serving.kserve.io/inferenceservice"]
+        namespace = pod.metadata.namespace
+        host_url = f"https://{model_name}-{namespace}.apps.albany.nerc.mghpcc.org"
+
+        set_config(model_name, host_url, namespace)
+
+        llm_load_test()
+        print(f"Load test for {model_name} completed ")
+
 
 def llm_load_test():
+    # Run llm-load-test
     try:
         subprocess.run(["python", "load_test.py"], check=True, cwd="llm-load-test")
     except Exception as e:
-        return "Error running load_test.py"
+        return f"Error running load_test.py: {e}"
 
 
 @metrics.route("/metrics", methods=["GET"])
 def export_metrics():
-    llm_load_test()
-
     output_file = "./llm-load-test/output/output-001.json"
 
     if os.path.exists(output_file):
@@ -47,4 +98,5 @@ def export_metrics():
 
 
 if __name__ == "__main__":
+    gather_model_info()
     metrics.run(host="0.0.0.0", port=8443)
